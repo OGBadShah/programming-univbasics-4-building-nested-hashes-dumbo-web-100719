045b44f9a2d8e0902f35ba0514e1bde36d96ed53
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-01 19:14:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,6 +38,7 @@ def monopoly_with_third_tier
 	}
 end
 
+
 def monopoly_with_fourth_tier
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
@@ -59,6 +60,5 @@ def monopoly_with_fourth_tier
 				:shortline_railroad=> {"mortgage_value"=> "$800"}
 			}
 	}
-
 
 end
