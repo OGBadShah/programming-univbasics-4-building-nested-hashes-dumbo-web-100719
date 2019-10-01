01c6d25bed6423c9a779ed2f8bcc5d3646dd8053
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-01 19:07:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	monopoly = {:railroads =>{}}
+	monopoly = {:railroads=> {}}
 end
 
 def monopoly_with_second_tier
@@ -10,8 +10,8 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		:railroads => {
-			:pieces => 4
+		:railroads=> {
+			:pieces=> 4
 		}
 	}
 end
@@ -22,18 +22,18 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		:railroads => {
-			:pieces => 4,
-			:rent_in_dollars  => {
-				:one_piece_owned => 25,
-				:two_pieces_owned => 50,
-				:three_pieces_owned => 100,
-				:four_pieces_owned => 200,
+		:railroads=> {
+			:pieces=> 4,
+			:rent_in_dollars=> {
+				:one_piece_owned=> 25,
+				:two_pieces_owned=> 50,
+				:three_pieces_owned=> 100,
+				:four_pieces_owned=> 200,
 			}
-			:names => {
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline_railroad => {}
+			:names=> {
+				:pennsylvania_railroad=> {},
+				:b_and_o_railroad=> {},
+				:shortline_railroad=> {}
 			}
 	}
 end
@@ -44,19 +44,19 @@ def monopoly_with_fourth_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	monopoly = {
-		:railroads => {
-			:pieces => 4,
-			:rent_in_dollars  => {
-				:one_piece_owned => 25,
-				:two_pieces_owned => 50,
-				:three_pieces_owned => 100,
-				:four_pieces_owned => 200,
+		:railroads=> {
+			:pieces=> 4,
+			:rent_in_dollars=> {
+				:one_piece_owned=> 25,
+				:two_pieces_owned=> 50,
+				:three_pieces_owned=> 100,
+				:four_pieces_owned=> 200,
 			}
-			:names => {
-				:reading_railroad => {"mortgage_value"},
-				:pennsylvania_railroad => {"mortgage_value"},
-				:b_and_o_railroad => {"mortgage_value"},
-				:shortline_railroad => {"mortgage_value"},
+			:names=> {
+				:reading_railroad=> {"mortgage_value"},
+				:pennsylvania_railroad=> {"mortgage_value"},
+				:b_and_o_railroad=> {"mortgage_value"},
+				:shortline_railroad=> {"mortgage_value"},
 			}
 	}
 
