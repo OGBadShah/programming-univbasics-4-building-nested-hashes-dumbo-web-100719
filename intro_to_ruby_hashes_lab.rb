e6275281594158d34e0103570048f200feb823d9
iamnabeel2 <iamnabeel2@gmail.com>
2019-10-01 19:08:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def monopoly_with_third_tier
 				:one_piece_owned=> 25,
 				:two_pieces_owned=> 50,
 				:three_pieces_owned=> 100,
-				:four_pieces_owned=> 200,
-			}
+				:four_pieces_owned=> 200
+			},
 			:names=> {
 				:pennsylvania_railroad=> {},
 				:b_and_o_railroad=> {},
@@ -50,8 +50,8 @@ def monopoly_with_fourth_tier
 				:one_piece_owned=> 25,
 				:two_pieces_owned=> 50,
 				:three_pieces_owned=> 100,
-				:four_pieces_owned=> 200,
-			}
+				:four_pieces_owned=> 200
+			},
 			:names=> {
 				:reading_railroad=> {"mortgage_value"},
 				:pennsylvania_railroad=> {"mortgage_value"},
